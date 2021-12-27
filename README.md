74276f3a8d9764de1e3c1c53758f80374c102a18
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-12-27 15:06:52 -0500

    Readme draft

---
+++ README.md
@@ -0,0 +1,5 @@
+#JWT CLI
+<img src="https://jwt.io/img/logo-asset.svg" align="center" alt="JWT"/>
+
+
+
---
+++ src/main/java/org/rajesh/jwtc/JWTC.java
@@ -0,0 +1,93 @@
+package org.rajesh.jwtc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+@Command(name = "jwtc", mixinStandardHelpOptions = true, version = "jwtc 0.1",
+        description = "Encode and decode JWT tokens.")
+@Slf4j
+public class JWTC implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "Specify encode or decode.")
+    String option;
+
+    @Parameters(index = "1", description = "User provided payload.")
+    String payload;
+
+    @Option(names = {"-s", "--secret"}, description = "Secret key.")
+    String secret;
+
+    @Option(names = {"-a", "--algorithm"}, description = "Algorithm to be used.")
+    String algorithm;
+
+    @Option(names = {"-t", "--ttl"}, description = "Time to Live in Milliseconds.")
+    long ttlMillis;
+
+    @Option(names = {"-b", "--base64"}, description = "Encode payload in Base64.")
+    boolean base64;
+
+    @Option(names = {"-jwt", "--jwt"}, description = "JWT to decode.")
+    String jwt;
+
+    @Override
+    public Integer call() throws Exception {
+        /* Start: Build Claims */
+        ObjectMapper payloadMapper = new ObjectMapper();
+        HashMap<Object, Object> payloadMap = new HashMap<>();
+        try {
+            //Convert payload JSON to Map
+            payloadMap = payloadMapper.readValue(payload, HashMap.class);
+        } catch (Exception e) {
+            log.error("Error while parsing payload JSON: {}", e.getMessage());
+        }
+
+        long nowMillis = System.currentTimeMillis();
+
+        payloadMap.put("iat", nowMillis);
+        if (ttlMillis > 0) {
+            payloadMap.put("exp", nowMillis + ttlMillis);
+        }
+        /* End: Build Claims */
+
+        /* Start: Build Header */
+        Map<String, Object> headerMap = new HashMap<>();
+        headerMap.put("alg", algorithm);
+        headerMap.put("typ", "JWT");
+        /* End: Build Header */
+
+        if (option.equals("encode")) {
+            log.info("Encoding claims: {}", payloadMap);
+            log.info("Using secret: {}", secret);
+            log.info("Using algorithm: {}", algorithm);
+
+            String jwt = JWTUtil.createJWT(payloadMap, secret, algorithm, ttlMillis);
+            log.info("JWT: {}", jwt);
+        } else if (option.equals("decode")) {
+            log.info("Decoding payload: {}", payloadMap);
+            log.info("Using secret: {}", secret);
+            log.info("Using algorithm: {}", algorithm);
+            String decodedJWT = JWTUtil.decodeJWT(jwt, secret).toString();
+            log.info("Decoded JWT: {}", decodedJWT);
+
+        } else {
+            log.error("Invalid option: {}", option);
+        }
+        return 0;
+    }
+
+    // this example implements Callable, so parsing, error handling and handling user
+    // requests for usage help or version help can be done with one line of code.
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new JWTC()).execute(args);
+        System.exit(exitCode);
+    }
+
+}
